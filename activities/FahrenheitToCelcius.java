a641c4f21d520a4bcdd16128fdc60542b55bf20d
Liam Completado <liambcompletado@gmail.com>
2025-10-22 14:12:31 +0800

    Update FahrenheitToCelcius.java

--- activities/FahrenheitToCelcius.java
+++ activities/FahrenheitToCelcius.java
@@ -9,7 +9,7 @@ public class FahrenheitToCelsius {
         System.out.print("Enter temperature in Fahrenheit: ");
         double fahrenheit = input.nextDouble();
 
-        double celcius = (fahrenheit - 32) * 5 / 9;
+        double celsius = (fahrenheit - 32) * 5 / 9;
         System.out.printf("Temperature in Celsius: %.2f °C", celsius);
 
         input.close();
