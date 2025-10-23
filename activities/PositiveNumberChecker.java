df33dac472c78634499bec3d028b4e37f477eece
Liam Completado <liambcompletado@gmail.com>
2025-10-24 00:02:17 +0800

    Update PositiveNumberChecker.java

--- activities/PositiveNumberChecker.java
+++ activities/PositiveNumberChecker.java
@@ -16,10 +16,9 @@ public class PositiveNumberChecker {
             }
 
         } while (number < 0);
-            
+
         System.out.println("Thank you! You entered: " + number);
 
         input.close();
     }
-    
 }
