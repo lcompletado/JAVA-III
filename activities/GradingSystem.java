8573520d91852db592cab902edf4eb9a9db723f2
Liam Completado <liambcompletado@gmail.com>
2025-10-24 23:27:01 +0800

    Update GradingSystem.java

--- activities/GradingSystem.java
+++ activities/GradingSystem.java
@@ -2,7 +2,7 @@ package activities;
 import java.util.Scanner;
 
 public class GradingSystem {
-    
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -10,21 +10,21 @@ public class GradingSystem {
         int score = input.nextInt();
 
         char grade;
-        
+
         if (score >= 90 && score <= 100) {
             grade = 'A';
-        } else if (score >= 80 && score < 90){
+        } else if (score >= 80){
             grade = 'B';
-        } else if (score >= 70 && score < 80) {
+        } else if (score >= 70) {
             grade = 'C';
-        } else if (score >= 60 && score < 70) {
+        } else if (score >= 60) {
             grade = 'D';
         } else {
             grade = 'F';
         }
 
         System.out.println("Your grade is " + grade);
-
         input.close();
     }
 }
+
